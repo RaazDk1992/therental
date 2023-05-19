7c3ddf7f2f48719f7ad50a57f8c07abe7a6b0bb7
RaazDk1992 <dhakal1992raaz@gmail.com>
2023-05-19 09:17:48 +0545

    multilingual-v1

--- lib/l10n/l10n.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class SupportList {
-  // ignore: non_constant_identifier_names
-  static final languageList = [const Locale('en'), const Locale('ne')];
-}
